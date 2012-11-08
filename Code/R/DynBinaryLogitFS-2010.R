cd1af893c4556ddd2dca3c646e44252ff2a96971
Jesse Windle <jesse.windle@gmail.com>
2012-11-07 19:24:17 -0600

    DynBinaryLogit-FS: new setup; now use FFBS.C.

--- Code/R/DynBinaryLogitFS-2010.R
+++ Code/R/DynBinaryLogitFS-2010.R
@@ -1,9 +1,13 @@
-## Dynamic BINARY logistic regression.
+## Dynamic BINARY logistic regression using FS's 2010 discrete mixture of
+## normals.
 
-## Independent AR(1)'s.  Maybe should change this.
-source("~/RV-Project/Code/C_Examples/MyLib/Gibbs/Ind/AR1/Stationary/Stationary.R");
-if (!is.loaded("FSF_nmix.so")) dyn.load("~/RPackage/BayesLogit/Code/R/FSF_nmix.so");
+if (!is.loaded("FSF_nmix.so")) dyn.load("../C/FSF_nmix.so");
 
+source("FFBS.R")
+source("Stationary.R"); ## Independent AR(1)'s.  Maybe should change this.
+
+################################################################################
+                              ## NORMAL MIXTURE ##
 ################################################################################
 
 ## This R script uses Fruhwirth-Schnatter and Fruhwirth's normal-mixture
@@ -67,92 +71,6 @@ NM = normal.mixture
 
 ################################################################################
 
-## When tracking (alpha, beta_t) or (\beta_t)
-FFBS <- function(z, X, mu, phi, W, r, m0, C0, NM, with.alpha=FALSE)
-{
-  ## When tracking (alpha, beta_t) or (\beta_t)
-  ## z_t = alpha + x_t beta_t + ep_t, ep_t \sim N(m_{r_i}, v_{r_i})
-  ## beta_t = mu + phi * (beta_t - mu) + omega_t, omega_t \sim N(0,W).
-  
-  ## z : vector of observations.
-  ## X : design matrix
-  ## phi : vector
-  ## r : mixture indicators : list
-  ## W : covariance MATRIX of innovations of beta.
-  ## m0 : prior mean on (alpha, beta)
-  ## C0 : prior var on (alpha, beta)
-
-  T = length(z);
-  N = ncol(X);
-  N.b  = length(phi);
-  N.a = N - N.b;
-  if (with.alpha) a.idc = 1:N.a;
-  b.idc = 1:N.b+N.a;
-  
-  m = array(m0, dim=c(N, T+1));
-  C = array(C0, dim=c(N, N, T+1));
-  R = array(0., dim=c(N, N, T+1));
-  a = array(0., dim=c(N, T+1));
-
-  beta = array(0, dim=c(N.b, T+1));
-  
-  d = c( rep(1, N.a), phi );
-  D = diag(d, N);
-  big.W = matrix(0, N, N); big.W[b.idc, b.idc] = W;
-  
-  ## Feed Forward
-  for (i in 2:(T+1)) {
-    i.l = i-1;
-
-    a[,i]  = d * m[,i-1] + (1-d) * mu;
-    R[,,i] = D %*% C[,,i-1] %*% D  + big.W;
-
-    tF.i = t(X[i.l,])
-    f.i  = tF.i %*% a[,i] + NM$m[r[i.l]];
-
-    Q    = tF.i %*% R[,,i] %*% t(tF.i) + diag(NM$v[r[i.l]], 1);
-    QI = solve(Q);
-
-    ## nm.p = 1 / NM$v[r[[i.l]]];
-    Rtx = R[,,i] %*% X[i.l,];
-    ## xRtx = X[i.l,] %*% Rtx;
-    ## QI  = diag(nm.p, n.i) - (nm.p / (1/xRtx + sum(nm.p))) %*% t(nm.p);
-
-    e.i = z[i.l] - f.i;
-
-    ## A.i = R[,,i] %*% t(tF.i) %*% QI;
-    A.i = Rtx %*% QI;
-
-    ## We could simplify further.
-    m[,i] = a[,i] + A.i %*% e.i;
-    ## C[,,i] = R[,,i] - A.i %*% Q %*% t(A.i);
-    C[,,i] = R[,,i] - Rtx %*% QI %*% t(Rtx);
-    
-  }
-
-  ## Backward Sample
-  ## L = t( chol(C[,,T+1]) );
-  evd = eigen(C[,,T+1]);
-  Rt = evd$vectors %*% diag(sqrt(evd$values), N) %*% t(evd$vectors);
-  theta = m[,T+1] + Rt %*% rnorm(N);
-  alpha = ifelse(with.alpha, theta[a.idc], 0);
-  beta[,T+1] = theta[b.idc];
-  
-  for (i in (T+1):2) {
-
-    B = C[,,i-1] %*% (solve(R[,,i]) * d);
-    theta.V = C[,,i-1] - B %*% R[,,i] %*% t(B);
-    L = t( chol(theta.V[b.idc, b.idc]) );
-    
-    e = beta[,i] - a[b.idc,i];
-    beta.m = m[b.idc,i-1] + B[b.idc, b.idc] %*% e;
-
-    beta[,i-1] = beta.m + L %*% rnorm(N.b);
-  }
-  
-  list("alpha"=alpha, "beta"=beta);
-} ## FFBS
-
 ##------------------------------------------------------------------------------
 
 draw.indicators <- function(z, lambda)
@@ -194,7 +112,7 @@ draw.z <- function(lambda, y){
 
 ################################################################################
 
-dyn.logit.FS <- function(y, X,
+dyn.logit.FS <- function(y, X.dyn, X.stc=NULL,
                          samp=1000, burn=100, verbose=100,
                          m.0=NULL, C.0=NULL,
                          mu.m0=NULL, mu.V0=NULL,
@@ -204,71 +122,107 @@ dyn.logit.FS <- function(y, X,
                          beta.true=NULL, iota.true=NULL,
                          mu.true=NULL, phi.true=NULL, W.true=NULL)
 {
-  ## y: the counts
-  ## X: the design matrix.
-
   ## m.0 = prior mean for (iota,beta_0) or (beta_0).
   ## C.0 = prior var  for (iota,beta_0) or (beta_0).
+
+  ## y: binary vector (T)
+  ## X: the design matrix (including covariates for non-dynamic coef.) (T x P)
+  ## n: the number of trials (T)
+
+  ## m.0: prior mean for (iota,beta_0) or (beta_0).  (P)
+  ## C.0: prior var  for (iota,beta_0) or (beta_0).  (P)
+
+  ## mu: mean of (beta_t) (P.b)
+  ## phi: ar coef. of (beta_t) (P.b)
+  ## W: innovation variance of (beta_t) (P.b) -- ASSUMED DIAGONAL.
+
+  ## A few things to keep in mind.
+  ## 1) phi = 1 ==> mu = 0.
+  ## 2) phi = 1 && X==1 ==> iota = 0.
+  ## 3) iota = unknown ==> beta = unknown.
+
+  ## Process for (alpha, beta_t):
+  ## alpha_t = alpha_{t-1}
+  ## beta_t \sim AR(1).
+  
+  ## NOTE: We do not combine data. ##
+
+  ## Structure.
+  y = as.matrix(y)
+  X = cbind(X.stc, X.dyn)
+  W = as.matrix(W);
+  C.0 = as.matrix(C.0);
   
   ## Dimension ##
-  y = as.matrix(y)
-  X = as.matrix(X)
-  N = nrow(X);
+  T = nrow(X);
   P = ncol(X);
+  P.b = ncol(X.dyn);
+  P.a = P - P.b
   M = samp;
-  
-  ## Default prior parameters ##
-  if (is.null(m.0))    m.0    = rep(0.0, P);
-  if (is.null(C.0))    C.0    = diag(1.0, P);
-  if (is.null(mu.m0))  mu.m0  = rep(0.0 , P);
-  if (is.null(mu.V0))  mu.V0  = rep(0.01, P);
-  if (is.null(phi.m0)) phi.m0 = rep(0.99, P);
-  if (is.null(phi.V0)) phi.V0 = rep(0.01, P);
-  if (is.null(W.a0))   W.a0   = rep(1.0, P);
-  if (is.null(W.b0))   W.b0   = rep(1.0, P);
+
+  ## Default prior parameters -- almost a random walk for beta ##
+  if (is.null(m.0)    || is.null(C.0))    { m.0    = rep(0.0, P)   ; C.0    = diag(1.0, P  ); }
+  if (is.null(mu.m0)  || is.null(mu.V0))  { mu.m0  = rep(0.0 ,P.b) ; mu.V0  = rep(0.01, P.b); }
+  if (is.null(phi.m0) || is.null(phi.V0)) { phi.m0 = rep(0.99,P.b) ; phi.V0 = rep(0.01, P.b); }
+  if (is.null(W.a0)   || is.null(W.b0))   { W.a0   = rep(1.0, P.b) ; W.b0   = rep(1.0,  P.b);  }
 
   ## Output data structure ##
   out = list(
-    z    = array(0, dim=c(M, N)),
-    r    = array(0, dim=c(M, N)),
-    iota = array(0, dim=c(M)),
-    beta = array(0, dim=c(M, P, N+1)),
-    mu   = array(0, dim=c(M, P)),
-    phi  = array(0, dim=c(M, P)),
-    W    = array(0, dim=c(M, P))
+    z    = array(0, dim=c(M, T)),
+    r    = array(0, dim=c(M, T)),
+    iota = array(0, dim=c(M, max(P.a, 1))),
+    beta = array(0, dim=c(M, P.b, T+1)),
+    mu   = array(0, dim=c(M, P.b)),
+    phi  = array(0, dim=c(M, P.b)),
+    W    = array(0, dim=c(M, P.b))
     )
 
   ## Initialize ##
-  beta = matrix(0.0, P, N+1);  # even odds
-  iota = 0
-  mu   = matrix(0.0, P);
-  phi  = matrix(0.99, P);
+  beta = matrix(0.00, P.b, T+1);  # even odds.
+  iota = rep(0.00, P.a);
+  mu   = matrix(mu.m0, P.b);
+  phi  = matrix(phi.m0, P.b);
   W    = W.b0 / W.a0;
   with.iota = TRUE
   nmix = NM
+  
+  ## In case we are doing testing or we want to constrain to local level model.
+  know.beta = FALSE;
+  know.z    = FALSE;
+  know.r    = FALSE;
+  know.phi  = FALSE;
+  know.mu   = FALSE;
+  know.W    = FALSE;
+  know.iota = FALSE;
 
-  ## In case we are doing testing.  May remove later.
-  if (!is.null(z.true))    z = z.true;
-  if (!is.null(r.true))    r = r.true;
-  if (!is.null(beta.true)) beta = beta.true;
-  if (!is.null(mu.true))   mu   = mu.true;
-  if (!is.null(phi.true))  phi  = phi.true;
-  if (!is.null(W.true))    W    = W.true;
-  if (!is.null(iota.true)) {iota = iota.true; with.iota=FALSE}
+  if (!is.null(beta.true)) { beta = beta.true; know.beta = TRUE; }
+  if (!is.null(z.true))    { z    = z.true;    know.z    = TRUE; }
+  if (!is.null(r.true))    { r    = r.true;    know.r    = TRUE; }
+  if (!is.null(phi.true))  { phi  = phi.true;  know.phi  = TRUE;
+                             if (phi==1) {mu.true = rep(0, P.b);}}
+  if (!is.null(mu.true))   { mu   = mu.true;   know.mu   = TRUE; }
+  if (!is.null(W.true))    { W    = W.true;    know.W    = TRUE; }
+  if (!is.null(iota.true)) { iota = iota.true; know.iota = TRUE; }
 
+  ## Check that we are okay.
+  if (know.beta && P.a > 0 && !know.iota) {printf("Know beta, not iota, X.stc!=NULL."); return(0);}
+
+  ## SAMPLE ##
+  
   start.time = proc.time();
   
   for (j in 1:(samp+burn)) {
     if (j==burn+1) start.ess = proc.time();
     
     ## JOINT DRAW: (z, r | beta, y) = (z | beta, y) (r | z, beta, y)
-    psi  = iota + apply(X * t(beta)[-1,], 1, sum)
+    psi = apply(X.dyn * t(beta)[-1,], 1, sum);
+    if (P.a > 0) psi = psi + X.stc %*% iota;
     lambda = exp(psi);
     z    = draw.z(lambda, y);
     r    = draw.indicators.C(z, lambda, nmix)
     
     ## (iota, beta | r, z, y)
-    ffbs = FFBS(z, X, mu, phi, diag(W,P), r, m.0, C.0, nmix, with.alpha=with.iota)
+    ffbs = FFBS.C(z, X, mu, phi, diag(W,P), nmix$v[r], m.0, C.0)
     iota = ffbs$alpha
     beta = ffbs$beta
 
@@ -316,7 +270,6 @@ if (FALSE) {
   N = nrow(X);
 
   ## Parameters
-  iota = 0;
   W   = 0.1;
   mu  = 2.0;
   phi = 0.95
@@ -338,7 +291,7 @@ if (FALSE) {
   }
 
   nmix = NM
-  psi = iota + apply(X * t(beta)[-1,], 1, sum); ## log-odds
+  psi = apply(X * t(beta)[-1,], 1, sum); ## log-odds
   p   = exp(psi) / (1 + exp(psi));
   r   = sample.int(nmix$N, N, replace=TRUE, prob=nmix$w);
   ep  = rnorm(N, 0, nmix$s[r]);
@@ -349,14 +302,14 @@ if (FALSE) {
   source("DynBinaryLogitFS-2010.R")
   samp = 300
   burn = 0
-  out <- dyn.logit.FS(y, X,
+  out <- dyn.logit.FS(y, X.dyn=X, X.stc=NULL,
                       samp=samp, burn=burn, verbose=100,
                       m.0=m0, C.0=C0,
                       mu.m0=NULL, mu.V0=NULL,
                       phi.m0=NULL, phi.V0=NULL,
                       W.a0=W.a0, W.b0=W.b0,
                       z.true=NULL, r.true=NULL,
-                      beta.true=NULL, iota.true=0,
+                      beta.true=NULL, iota.true=NULL,
                       mu.true=0.0, phi.true=1.0, W.true=NULL)
 
   ess = apply(out$beta[, 1, ], 2, ESS);
