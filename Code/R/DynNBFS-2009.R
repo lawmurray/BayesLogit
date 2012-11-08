52b393b5c6a01aefaf66aef38fe96ffde514e9a9
Jesse Windle <jesse.windle@gmail.com>
2012-11-07 19:28:39 -0600

    Same changes to Dyn NB FS.

--- Code/R/DynNBFS-2009.R
+++ Code/R/DynNBFS-2009.R
@@ -1,161 +1,15 @@
-
 ## Negative binomial regression using FSF's log-gamma parameterization.
 ## We model the log-mean here.
 
-## Independent AR(1)'s.  Maybe should change this.
-source("~/RV-Project/Code/C_Examples/MyLib/Gibbs/Ind/AR1/Stationary/Stationary.R");
-source("~/RPackage/BayesLogit/Code/R/ComputeMixture.R")
-if (!is.loaded("FSF_nmix.so")) dyn.load("~/RPackage/BayesLogit/Code/R/FSF_nmix.so");
+if (!is.loaded("FSF_nmix.so")) dyn.load("../C/FSF_nmix.so");
+
+source("ComputeMixture.R")
+source("FFBS.R")
+source("NBDF.R")
+source("Stationary.R"); ## Independent AR(1)'s.  Maybe should change this.
+
 
 ################################################################################
-
-## When tracking (alpha, beta_t) or (\beta_t)
-FFBS <- function(z, X, mu, phi, W, r, m0, C0, NM, with.alpha=FALSE)
-{
-  ## When tracking (alpha, beta_t) or (\beta_t)
-  ## z_t = alpha + x_t beta_t + ep_t, ep_t \sim N(m_{r_i}, v_{r_i})
-  ## beta_t = mu + phi * (beta_t - mu) + omega_t, omega_t \sim N(0,W).
-  
-  ## z : vector of observations.
-  ## X : design matrix
-  ## phi : vector
-  ## r : mixture indicators : list
-  ## W : covariance MATRIX of innovations of beta.
-  ## m0 : prior mean on (alpha, beta)
-  ## C0 : prior var on (alpha, beta)
-
-  T = length(z);
-  N = ncol(X);
-  N.b  = length(phi);
-  N.a = N - N.b;
-  if (with.alpha) a.idc = 1:N.a;
-  b.idc = 1:N.b+N.a;
-  
-  m = array(m0, dim=c(N, T+1));
-  C = array(C0, dim=c(N, N, T+1));
-  R = array(0., dim=c(N, N, T+1));
-  a = array(0., dim=c(N, T+1));
-
-  beta = array(0, dim=c(N.b, T+1));
-  
-  d = c( rep(1, N.a), phi );
-  D = diag(d, N);
-  big.W = matrix(0, N, N); big.W[b.idc, b.idc] = W;
-  
-  ## Feed Forward
-  for (i in 2:(T+1)) {
-    i.l = i-1;
-
-    a[,i]  = d * m[,i-1] + (1-d) * mu;
-    R[,,i] = D %*% C[,,i-1] %*% D  + big.W;
-
-    tF.i = t(X[i.l,])
-    f.i  = tF.i %*% a[,i] + NM$m[r[i.l]];
-
-    Q    = tF.i %*% R[,,i] %*% t(tF.i) + diag(NM$v[r[i.l]], 1);
-    QI = solve(Q);
-
-    ## nm.p = 1 / NM$v[r[[i.l]]];
-    Rtx = R[,,i] %*% X[i.l,];
-    ## xRtx = X[i.l,] %*% Rtx;
-    ## QI  = diag(nm.p, n.i) - (nm.p / (1/xRtx + sum(nm.p))) %*% t(nm.p);
-
-    e.i = z[i.l] - f.i;
-
-    ## A.i = R[,,i] %*% t(tF.i) %*% QI;
-    A.i = Rtx %*% QI;
-
-    ## We could simplify further.
-    m[,i] = a[,i] + A.i %*% e.i;
-    ## C[,,i] = R[,,i] - A.i %*% Q %*% t(A.i);
-    C[,,i] = R[,,i] - Rtx %*% QI %*% t(Rtx);
-    
-  }
-
-  ## Backward Sample
-  ## L = t( chol(C[,,T+1]) );
-  evd = eigen(C[,,T+1]);
-  Rt = evd$vectors %*% diag(sqrt(evd$values), N) %*% t(evd$vectors);
-  theta = m[,T+1] + Rt %*% rnorm(N);
-  alpha = ifelse(with.alpha, theta[a.idc], 0);
-  beta[,T+1] = theta[b.idc];
-  
-  for (i in (T+1):2) {
-
-    B = C[,,i-1] %*% (solve(R[,,i]) * d);
-    theta.V = C[,,i-1] - B %*% R[,,i] %*% t(B);
-    L = t( chol(theta.V[b.idc, b.idc]) );
-    
-    e = beta[,i] - a[b.idc,i];
-    beta.m = m[b.idc,i-1] + B[b.idc, b.idc] %*% e;
-
-    beta[,i-1] = beta.m + L %*% rnorm(N.b);
-  }
-  
-  list("alpha"=alpha, "beta"=beta);
-} ## FFBS
-
-##------------------------------------------------------------------------------
-
-df.llh <- function(d, mu, G, ymax)
-{
-  p =  1 / (1 + d / mu)
-  sum( log(d+0:(ymax-1)) * G[1:ymax] ) + d * sum(log(1-p)) + sum(y * log(p)) ;
-}
-
-draw.df <- function(d.prev, mu, G, ymax)
-{
-  ## optim.out <- optim(d.prev, fn=df.llh, gr = NULL,
-  ##                    mu, G, ymax,                              ## for minimization
-  ##                    method="L-BFGS-B", lower=1, hessian=TRUE, control=list(fnscale=-1));
-  
-  ## mle = optim.out$par
-  ## fim = -1 / optim.out$hessian
-
-  ## cat("d.prev:", d.prev, "mle:", mle, "fim:", fim, "\n");
-
-  d.new = d.prev
-  
-  ## Mixture of MH kernels.
-  w = c(0.5, 0.5);
-  ## k = sample.int(2, 1, prob=w);
-  k = 1
-  
-  if (k==1) {
-    ## Kernel 1: RW MH.
-    rw.lower = max(d.prev - 1, 1);
-    rw.upper = d.prev + 1;
-    rw.grid  = rw.lower:rw.upper;
-    rw.n     = length(rw.grid)
-    rw.p     = rep(1/rw.n, rw.n);
-    
-    d.prop = sample(rw.grid, 1, prob=rw.p);
-    
-    ltarget = df.llh(d.prop, mu, G, ymax) - df.llh(d.prev, mu, G, ymax)
-    lppsl = log(ifelse(d.prop==1, 1/2, 1/3)) - log(ifelse(d.prev==1, 1/2, 1/3));
-    lratio = ltarget + lppsl
-    
-    if (runif(1) < exp(lratio)) d.new = d.prop
-  }
-
-  if (k==2) {
-    ## Kernel 2: Ind MH.
-    d.m = max(1, mle);
-    d.prop = rpois(1, d.m);
-
-    if (d.prop==0) return(d.prev);
-    
-    p.prop = 1 / (1 + d.prop / mu)
-    p.prev = 1 / (1 + d.prev / mu)
-    ltarget = df.llh(d.prop, mu, G, ymax) - df.llh(d.prev, mu, G, ymax)
-    lppsl   = dpois(d.prev, d.m, log=TRUE) - dpois(d.prop, d.m, log=TRUE)
-    lratio  = ltarget + lppsl
-
-    if (runif(1) < exp(lratio)) d.new = d.prop
-  }
-    
-  d.new
-}
 
 ##------------------------------------------------------------------------------
 
@@ -189,9 +43,11 @@ draw.indicators.C <- function(res, nmix)
   OUT[[1]]
 } ## draw.indicators.C
 
+################################################################################
+
 ##------------------------------------------------------------------------------
 
-dyn.NB.FS <- function(y, X,
+dyn.NB.FS <- function(y, X.dyn, X.stc=NULL,
                       samp=1000, burn=100, verbose=100000,
                       m.0=NULL, C.0=NULL,
                       mu.m0=NULL, mu.V0=NULL,
@@ -201,74 +57,107 @@ dyn.NB.FS <- function(y, X,
                       beta.true=NULL, iota.true=NULL,
                       mu.true=NULL, phi.true=NULL, W.true=NULL)
 {
-  ## y: N by 1 vector, avg response
-  ## X: N by P matrix
-
   ## m.0 = prior mean for (iota,beta_0) or (beta_0).
   ## C.0 = prior var  for (iota,beta_0) or (beta_0).
+
+  ## y: the average response (T)
+  ## X: the design matrix (including covariates for non-dynamic coef.) (T x P)
+  ## n: the number of trials (T)
+
+  ## m.0: prior mean for (iota,beta_0) or (beta_0).  (P)
+  ## C.0: prior var  for (iota,beta_0) or (beta_0).  (P)
+
+  ## mu: mean of (beta_t) (P.b)
+  ## phi: ar coef. of (beta_t) (P.b)
+  ## W: innovation variance of (beta_t) (P.b) -- ASSUMED DIAGONAL.
+
+  ## A few things to keep in mind.
+  ## 1) phi = 1 ==> mu = 0.
+  ## 2) phi = 1 && X==1 ==> iota = 0.
+  ## 3) iota = unknown ==> beta = unknown.
+
+  ## Process for (alpha, beta_t):
+  ## alpha_t = alpha_{t-1}
+  ## beta_t \sim AR(1).
+  
+  ## Structure.
+  y = as.matrix(y)
+  X = cbind(X.stc, X.dyn)
+  W = as.matrix(W);
+  C.0 = as.matrix(C.0);
   
   ## Dimension ##
-
-  y = as.matrix(y)
-  X = as.matrix(X)
-  N = nrow(X);
+  T = nrow(X);
   P = ncol(X);
-  M = samp
-  with.iota = is.null(iota.true) ## Maybe should adjust prior.
-  
-  ## Default prior parameters.
-  if (is.null(m.0))    m.0    = rep(0.0, P);
-  if (is.null(C.0))    C.0    = diag(1.0, P);
-  if (is.null(mu.m0))  mu.m0  = rep(0.0 , P);
-  if (is.null(mu.V0))  mu.V0  = rep(0.01, P);
-  if (is.null(phi.m0)) phi.m0 = rep(0.99, P);
-  if (is.null(phi.V0)) phi.V0 = rep(0.01, P);
-  if (is.null(W.a0))   W.a0   = rep(1.0, P);
-  if (is.null(W.b0))   W.b0   = rep(1.0, P);
+  P.b = ncol(X.dyn);
+  P.a = P - P.b
+  M = samp;
+
+  ## Default prior parameters -- almost a random walk for beta ##
+  if (is.null(m.0)    || is.null(C.0))    { m.0    = rep(0.0, P)   ; C.0    = diag(1.0, P  ); }
+  if (is.null(mu.m0)  || is.null(mu.V0))  { mu.m0  = rep(0.0 ,P.b) ; mu.V0  = rep(0.01, P.b); }
+  if (is.null(phi.m0) || is.null(phi.V0)) { phi.m0 = rep(0.99,P.b) ; phi.V0 = rep(0.01, P.b); }
+  if (is.null(W.a0)   || is.null(W.b0))   { W.a0   = rep(1.0, P.b) ; W.b0   = rep(1.0,  P.b); }
 
   ## Output data structure.
   out = list(
-    iota = array(0, dim=c(M)),
-    beta = array(0, dim=c(M, P, N+1)),
-    lambda = array(0, dim=c(M, N)),
-    r    = array(0, dim=c(M, N)),
-    mu   = array(0, dim=c(M, P)),
-    phi  = array(0, dim=c(M, P)),
-    W    = array(0, dim=c(M, P))
+    d    = array(0, dim=c(M)),
+    iota   = array(0, dim=c(M, max(P.a, 1))),
+    beta   = array(0, dim=c(M, P.b, T+1)),
+    lambda = array(0, dim=c(M, T)),
+    r      = array(0, dim=c(M, T)),
+    mu     = array(0, dim=c(M, P.b)),
+    phi    = array(0, dim=c(M, P.b)),
+    W      = array(0, dim=c(M, P.b))
     )
 
   ## Initialize ##
-  beta = matrix(0.0, P, N+1);  ## mean = 1.
-  iota = 0.0
   d    = 1
-  mu   = matrix(mu.m0, P);
-  phi  = matrix(phi.m0, P);
-  W    = rep(W.b0 / W.a0, P)
+  beta = matrix(0.00, P.b, T+1);  # mean = 1
+  iota = rep(0.00, P.a);
+  mu   = matrix(mu.m0, P.b);
+  phi  = matrix(phi.m0, P.b);
+  W    = rep(W.b0 / W.a0, P.b);
   
   ## In case we are doing testing or we want to constrain to local level model.
-  if (!is.null(d.true))    d    = d.true;
-  if (!is.null(r.true))    r    = r.true;
-  if (!is.null(lambda.true)) lambda = lambda.true;
-  if (!is.null(iota.true)) {iota = iota.true; with.iota = FALSE}
-  if (!is.null(beta.true)) beta = beta.true;
-  if (!is.null(mu.true))   mu   = mu.true;
-  if (!is.null(phi.true))  phi  = phi.true;
-  if (!is.null(W.true))    W    = W.true;
+  know.d    = FALSE;
+  know.beta = FALSE;
+  know.lambda = FALSE;
+  know.r    = FALSE;
+  know.phi  = FALSE;
+  know.mu   = FALSE;
+  know.W    = FALSE;
+  know.iota = FALSE;
+
+  if (!is.null(d.true))    { d    = d.true;    know.d    = TRUE; }
+  if (!is.null(beta.true)) { beta = beta.true; know.beta = TRUE; }
+  if (!is.null(lambda.true)) { lambda = lambda.true; know.lambda = TRUE; }
+  if (!is.null(r.true))    { r    = r.true;    know.r    = TRUE; }
+  if (!is.null(phi.true))  { phi  = phi.true;  know.phi  = TRUE;
+                             if (phi==1) {mu.true = rep(0, P.b);}}
+  if (!is.null(mu.true))   { mu   = mu.true;   know.mu   = TRUE; }
+  if (!is.null(W.true))    { W    = W.true;    know.W    = TRUE; }
+  if (!is.null(iota.true)) { iota = iota.true; know.iota = TRUE; }
+
+  ## Check that we are okay.
+  if (know.beta && P.a > 0 && !know.iota) {printf("Know beta, not iota, X.stc!=NULL."); return(0);}
 
   ## Preprocess ## 
   ymax = max(y);
   F = cumsum(hist(y, breaks=0:(ymax+1)-0.5, plot=FALSE)$counts)
-  G = N - F;
+  G = T - F;
   
   start.time = proc.time();
 
   ## SAMPLE ##
+   
   for ( j in 1:(samp+burn) )
   {
     if (j==burn+1) start.ess = proc.time();
 
     ## draw (d, lambda, r | beta) --- WARNING: JOINT DRAW.
-    log.mean  = iota + apply(X * t(beta)[-1,], 1, sum)
+    log.mean = apply(X.dyn * t(beta)[-1,], 1, sum);
+    if (P.a > 0) psi = log.mean + X.stc %*% iota;
     mu.lambda = exp(log.mean)
     
     ## draw (d | beta)
@@ -277,7 +166,7 @@ dyn.NB.FS <- function(y, X,
     ## draw (lambda | d, beta)
     psi = log.mean - log(d);
     p = 1 / (1 + exp(-psi))
-    lambda = rgamma(N, y+d, scale=p)
+    lambda = rgamma(T, y+d, scale=p)
 
     ## draw (r | d, lambda, beta)
     nmix = compute.mixture(d);
@@ -285,9 +174,8 @@ dyn.NB.FS <- function(y, X,
     r    = draw.indicators.C(res, nmix);
 
     ## draw beta
-    nmix$m = -1 * nmix$m;     ## We want log Gamma not - log Gamma.
-    z = log(lambda) + log(d); ## So that we model the log mean.
-    ffbs = FFBS(z, X, mu, phi, diag(W,P), r, m.0, C.0, nmix, with.alpha=with.iota)
+    z = log(lambda) + log(d) + nmix$m[r]; ## So that we model the log mean.
+    ffbs = FFBS.C(z, X, mu, phi, diag(W,P.b), nmix$v[r], m.0, C.0)
     iota = ffbs$alpha
     beta = ffbs$beta
 
@@ -327,21 +215,20 @@ if (FALSE) {
   dyn.load("FSF_nmix.so")
   
   T = 500;
-  N = 1;
+  P = 1;
 
-  beta = array(0, dim=c(N, T+1));
-  X = matrix(1, T, N);
+  beta = array(0, dim=c(P, T+1));
+  X = matrix(1, T, P);
 
   ## Parameters
   W = 0.1;
   phi = 0.95;
-  mu = 1.0
+  mu = 0.0
   d = 4
   nmix = compute.mixture(d)
-  iota = 0
 
   ## Prior
-  m.0     = mu;
+  m.0     = 1.0;
   C.0     = 4.0;
   mu.m0  = 0.0;
   mu.V0  = 4.0;
@@ -351,12 +238,12 @@ if (FALSE) {
   W.b0   = 1.0;
 
   ## Synthetic
-  beta[,1] = m0;
+  beta[,1] = m.0;
   for (i in 2:(T+1)) {
     beta[,i] = mu + phi* (beta[,i-1] - mu) + sqrt(W) * rnorm(1);
   }
 
-  log.mean  = iota + apply(X * t(beta)[-1,], 1, sum)
+  log.mean  = apply(X * t(beta)[-1,], 1, sum)
   psi       = log.mean - log(d)
   mu.lambda = exp(log.mean)
 
@@ -367,162 +254,31 @@ if (FALSE) {
   
   ## Simulate ##
   source("DynNBFS-2009.R")
-  samp = 500; burn=0; 
-  out <- dyn.NB.FS(y, X,
+  samp = 500; burn=0;
+  out <- dyn.NB.FS(y, X.dyn=X, X.stc=NULL,
                    samp=samp, burn=burn, verbose=50,
                    m.0=m.0, C.0=C.0,
                    mu.m0=mu.m0, mu.V0=mu.V0,
                    phi.m0=phi.m0, phi.V0=phi.V0,
                    W.a0=W.a0, W.b0=W.b0,
                    d.true=NULL, lambda.true=NULL, r.true=NULL, 
-                   beta.true=NULL, iota.true=iota,
+                   beta.true=NULL, iota.true=NULL,
                    mu.true=mu, phi.true=1.0, W.true=NULL)
+  
 }
 
 if (FALSE) {
 
-  beta.m = array(0, dim=c(N, T+1));
-  for (i in 1:N) {
+  beta.m = array(0, dim=c(P, T+1));
+  for (i in 1:P) {
     beta.m[i,] = apply(out$beta[,i,], 2, mean);
   }
 
   ymin = min(beta.m, beta);
   ymax = max(beta.m, beta);
 
-  plot(beta[1,], type="l", ylim=c(0, ymax))
+  plot(beta[1,], type="l", ylim=c(ymin, ymax))
   lines(beta.m[1,], col=2)
   points(log(y), col="gray")
   
 }
-
-################################################################################
-                                 ## APPENDIX ##
-################################################################################
-
-## Regarding the FFBS below.  This was adapted from <DynLogitPG.R>.  It is
-## essentially identical.  I changed it so that it could accomodate the normal
-## mixture, but the PG case is just commented out.  It is easy to go between the
-## two.
-
-## The original appiclation was binary logistic, but it turns out the FFBS is
-## essentially the same.  It runs slow because I have adapted things so that you
-## can have missing data.  I did this to try and accomodate the advertising
-## dataset, which is irregularly spaced in the ``time'' variable, which I
-## believe was duration of the phone call or soemthing like that.
-
-## You can use a local level model by simple setting mu to 0 and phi to 1.
-## Otherwise you can use a stationary AR(1) process.
-
-## Old FFBS
-## ## FFBS.dyn
-## FFBS.dyn <- function(y.u, X, tpred, mu, phi, W, m0, C0, tm, Omega, r=NULL)
-## {
-##   ## y: vector: y^u
-##   ## X: matrix: Design Matrix
-##   ## mu: vector: mean, if it exists.
-##   ## phi: vector:  autoregression parameter
-##   ## r: matrix: mixture index.
-##   ## W: vector: variance of omega_t, i.e. omega_t \sim N(0, diag(W))
-##   ## m0: vector: mean of beta_0.
-##   ## C0: matrix: var  of beta_0.
-##   ## tm: maps time to index.
-
-##   K = length(tm);
-##   T = tpred[tm[K]];
-##   P = ncol(X);
-##   N = nrow(X);
-
-##   m = array(m0, dim=c(P, T+1));
-##   C = array(C0, dim=c(P, P, T+1));
-##   R = array(0., dim=c(P, P, T+1));
-##   a = array(0., dim=c(P, T+1));
-##   UR = array(0., dim=c(P, P, T+1));
-
-##   beta = array(0, dim=c(P, T+1));
-
-##   Phi = diag(phi, P);
-##   idx = 1;
-  
-##   ## FEED FORWARD ##
-##   for (i in 1:T) {
-##     i.h = i+1 # Index of beta (and all other variables that start at 0)
-##     a[,i.h]  = (1 - phi) * mu + phi * m[,i.h-1];
-##     R[,,i.h] = Phi %*% (C[,,i.h-1] * phi) + W;
-##     UR[,,i.h] = chol(R[,,i.h]);
-
-##     ## Check if we have an observation.
-##     if (tpred[tm[idx]]==i) {
-##       ## Indicies and pick correct parts of y, X, and r.
-##       n.i = ifelse(idx==K, N+1 - tm[idx], tm[idx+1] - tm[idx]);
-##       idc = tm[idx]:(tm[idx] + n.i - 1);
-##       y.i = y.u[idc];
-##       X.i = matrix(X[idc,], nrow=n.i);
-      
-##       ## Mean and Variance of nu -- CAN CHANGE DEPENDING PG OR FS.
-##       ## omega.i = Omega[idc];
-##       ## b.i = rep(0, length(omega.i));
-##       ## V.i = 1.0 / omega.i
-##       r.i = r[idc];
-##       b.i = -1 * NM$m[r.i];
-##       V.i = NM$v[r.i];
-
-##       ## Forecast
-##       f.i = X.i %*% a[,i.h] + b.i;
-##       rho.i = X.i %*% R[,,i.h];
-##       e.i = y.i - f.i;
-      
-##       ## Joint to Conditional
-##       ## if (n.i > 1.5 * P) {
-##         ## tXdivV = t(X.i) / V.i;
-##         ## M  = chol2inv(UR[,,i.h]) + tXdivV %*% X.i;
-##         ## L  = t(chol(M));
-##         ## xi = backsolve(t(L), tXdivV)
-##         ## QInv = diag(1/V.i, n.i) - t(xi) %*% xi;
-##         ## A.i  = t(rho.i) %*% QInv;
-##         ## m[,i.h]  = a[,i.h] + A.i %*% e.i;
-##         ## C[,,i.h] = R[,,i.h] - A.i %*% rho.i;
-##       ## }
-##       ## else {
-##         Q.i = rho.i %*% t(X.i) + diag(V.i, n.i);
-##         L  = t(chol(Q.i));
-##         xi = forwardsolve(L, rho.i);
-##         tA.i  = backsolve(t(L), xi);
-##         m[,i.h]  = a[,i.h] + t(tA.i) %*% e.i;
-##         C[,,i.h] = R[,,i.h] - t(xi) %*% xi;
-##       ## }
-
-##       idx = idx + 1;
-##     }
-##     else {
-
-##       ## cat("Warning: missing data at time", i, ".\n");
-##       m[,i.h]  = a[,i.h]
-##       C[,,i.h] = R[,,i.h]
-      
-##     }
-
-##   }
-
-##   ## BACKWARDS SAMPLE ##
-##   i.h = T+1;
-##   L = t(chol(C[,,i.h]));
-##   beta[,i.h] = m[,i.h] + L %*% rnorm(P);
-
-##   for (i in (T+1):2) {
-##     i.y = i-1;
-##     rho.i = Phi %*% C[,,i-1];
-##     xi = forwardsolve(t(UR[,,i]), rho.i);
-##     tB.i = backsolve(UR[,,i], xi);
-##     e.i  = beta[,i] - a[,i];
-##     ell  = m[,i-1] + t(tB.i) %*% e.i;
-##     U    = C[,,i-1] - t(xi) %*% xi;
-
-##     L = t(chol(U));
-##     beta[,i-1] = ell + L %*% rnorm(P);
-    
-##   }
-
-##   out = list("beta"=beta, "m"=m)
-  
-##   out
-## } ## FFBS.dyn
