20972a8f7bd40a7355fc946587759cb942c0503e
Jesse Windle <jesse.windle@gmail.com>
2012-11-08 21:23:09 -0600

    Altered DESCRIPTION and NAMESPACE and sync.bash to include new files/routines.

--- Code/BLPackage/sync.bash
+++ Code/BLPackage/sync.bash
@@ -1,40 +1,49 @@
 #!/bin/bash
 # Copy the necessary files to the BayesLogit Package directory.
 
+BASE=/Users/jwindle/RPackage/BayesLogit
+
 rsyncit="rsync -Crvzut --exclude-from=$HOME/.rsync-exclude"
 
-BASE=/Users/jwindle/RPackage/BayesLogit/Code
-MYLIB=/Users/jwindle/RV-Project/Code/C_Examples/MyLib
+CODE=$BASE/Code
+INCL=$CODE/C/include
 
-BLDIR=/Users/jwindle/RPackage/BayesLogit/Code/BLPackage/BayesLogit
+BLDIR=$CODE/BLPackage/BayesLogit
 
 # CPP files.
-$rsyncit $BASE/C/Logit.hpp        $BLDIR/src/Logit.h
-$rsyncit $BASE/C/MultLogit.hpp    $BLDIR/src/MultLogit.h
-$rsyncit $BASE/C/LogitWrapper.hpp $BLDIR/src/LogitWrapper.h
-$rsyncit $BASE/C/LogitWrapper.cpp $BLDIR/src/
-$rsyncit $BASE/C/PolyaGamma.hpp   $BLDIR/src/PolyaGamma.h
+$rsyncit $CODE/C/Logit.hpp        $BLDIR/src/Logit.h
+$rsyncit $CODE/C/MultLogit.hpp    $BLDIR/src/MultLogit.h
+$rsyncit $CODE/C/LogitWrapper.hpp $BLDIR/src/LogitWrapper.h
+$rsyncit $CODE/C/LogitWrapper.cpp $BLDIR/src/
+$rsyncit $CODE/C/PolyaGamma.hpp   $BLDIR/src/PolyaGamma.h
+$rsyncit $CODE/C/FFBS.h           $BLDIR/src/
+$rsyncit $CODE/C/FFBS.cpp         $BLDIR/src/
+$rsyncit $CODE/C/FSF_nmix.hpp     $BLDIR/src/FSF_nmix.h
+$rsyncit $CODE/C/FSF_nmix.cpp     $BLDIR/src/
 
-$rsyncit $MYLIB/Matrix/Matrix.h             $BLDIR/src/
-$rsyncit $MYLIB/Matrix/MatrixFrame.h        $BLDIR/src/
-$rsyncit $MYLIB/RNG/RNG.hpp                 $BLDIR/src/RNG.h
-$rsyncit $MYLIB/RNG/RRNG.hpp                $BLDIR/src/RRNG.h
-$rsyncit $MYLIB/RandomVariates/Normal.hpp   $BLDIR/src/Normal.h
+$rsyncit $INCL/Matrix/Matrix.h             $BLDIR/src/
+$rsyncit $INCL/Matrix/MatrixFrame.h        $BLDIR/src/
+$rsyncit $INCL/RNG/RNG.hpp                 $BLDIR/src/RNG.h
+$rsyncit $INCL/RNG/RNG.cpp                 $BLDIR/src/
+$rsyncit $INCL/RNG/RRNG.hpp                $BLDIR/src/RRNG.h
+$rsyncit $INCL/RNG/RRNG.cpp                $BLDIR/src/
+$rsyncit $INCL/Normal.hpp                  $BLDIR/src/Normal.h
 
 # R files.
-$rsyncit $BASE/R/LogitWrapper.R  $BLDIR/R/
-$rsyncit $BASE/R/KS.R            $BLDIR/R/
+$rsyncit $CODE/R/LogitWrapper.R     $BLDIR/R/
+$rsyncit $CODE/R/NB-Indicators.R    $BLDIR/R/
+$rsyncit $CODE/R/Logit-Indicators.R $BLDIR/R/
+$rsyncit $CODE/R/FFBS.R             $BLDIR/R/
+$rsyncit $CODE/R/KS.R               $BLDIR/R/
 ## The function calls in these files are not in the NAMESPACE.
-$rsyncit $BASE/R/LogitPG.R    $BLDIR/R/
-$rsyncit $BASE/R/logit-EM.R      $BLDIR/R/
-$rsyncit $BASE/R/logit-combine.R $BLDIR/R/
-$rsyncit $BASE/R/PG.R            $BLDIR/R/
-$rsyncit $BASE/R/MultLogitPG.R   $BLDIR/R/
+$rsyncit $CODE/R/LogitPG.R       $BLDIR/R/
+$rsyncit $CODE/R/logit-EM.R      $BLDIR/R/
+$rsyncit $CODE/R/logit-combine.R $BLDIR/R/
+$rsyncit $CODE/R/PG.R            $BLDIR/R/
+$rsyncit $CODE/R/MultLogitPG.R   $BLDIR/R/
 
 # Data files.
-$rsyncit $BASE/R/DataSets/spambase.RData      $BLDIR/data/
-
-sed -i~ s/\"Logit\"/\"BayesLogit\"/ $BLDIR/R/LogitWrapper.R
+$rsyncit $CODE/R/DataSets/spambase.RData      $BLDIR/data/
 
 # Change to .h
 sed -i~ s/\.hpp/.h/g $BLDIR/src/*.[ch]
