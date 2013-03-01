36b27755ad6f4cd1a3e1d59a43f823d7b02ec879
Jesse Windle <jesse.windle@gmail.com>
2013-02-28 20:48:57 -0600

    Set bash script so that you .Rd files for dynamic functions can be moved to a from man.

--- Code/BLPackage/BayesLogit/inst/set.bash
+++ Code/BLPackage/BayesLogit/inst/set.bash
@@ -14,3 +14,28 @@ set -x
 cp ${RELDIR}${1}/DESCRIPTION ${RELDIR}../DESCRIPTION
 cp ${RELDIR}${1}/NAMESPACE   ${RELDIR}../NAMESPACE
 cp ${RELDIR}${1}/Makevars    ${RELDIR}../src/Makevars
+
+set +x
+
+FILES="CUBS.Rd AR1.Rd AR1Ind.Rd"
+
+## Add files for dynamic.
+if [[ $1 == Dynamic ]]; then
+  for FILE in $FILES; do
+	set -x
+    cp "${RELDIR}${1}/$FILE" "${RELDIR}../man/$FILE"
+	set +x
+  done
+fi
+
+## Remove files for static.
+if [[ $1 == Static ]]; then
+  for FILE in $FILES; do
+	PTF="${RELDIR}../man/$FILE"
+	if [[ -e "$PTF" ]]; then
+	  set -x
+	  rm "$PTF"
+	  set +x
+    fi
+  done	
+fi
