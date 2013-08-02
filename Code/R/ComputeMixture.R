afad1da164c8f46f045963acbd830161d2a1a013
Jesse Windle <jesse.windle@gmail.com>
2013-08-02 17:19:27 -0400

    Removed copyright from compmix and ComputeMixture.R

--- Code/R/ComputeMixture.R
+++ Code/R/ComputeMixture.R
@@ -1,8 +1,3 @@
-## Copyright 2013 Nick Polson, James Scott, and Jesse Windle.
-
-## This file is part of BayesLogit, distributed under the GNU General Public
-## License version 3 or later and without ANY warranty, implied or otherwise.
-
 ## Approximation of the negative log-gamma distribution by Gaussian mixtures
 ## Compute mixture parameters, given degrees of freedom
 
--- Code/R/compmix.R
+++ Code/R/compmix.R
@@ -1,8 +1,3 @@
-## Copyright 2013 Nick Polson, James Scott, and Jesse Windle.
-
-## This file is part of BayesLogit, distributed under the GNU General Public
-## License version 3 or later and without ANY warranty, implied or otherwise.
-
 ## This R function is taken from the binomlogit package.
 
 # Approximation of the type III generalized logistic distribution by Gaussian mixtures
